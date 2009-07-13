e8ca0bfc1ed28d02ab6c78725627d52f05bc40c2
pstevens <ph.stevens@gmx.de>
2009-07-14 00:04:47 +0200

    Search fixed, robustheit in imports

--- Search.py
+++ Search.py
@@ -4,6 +4,8 @@ from Genome import Genome
 import re
 from GenomeModel import GenomeModel
 from TreeView import TreeView
+from FeatureList import FeatureList
+from Feature import Feature
 class Search(object):
     """contains search moduls"""
 
@@ -15,7 +17,7 @@ class Search(object):
     def regexsearch(self, patterns):
         """search in a string with a re"""
         genome = Imports.genome
-        regex = re.compile(patterns , re.I)
+        regex = re.compile(patterns)
         matches = regex.finditer(genome.getSequence())
         regexlist = FeatureList("regex")
         if matches is None:
@@ -25,7 +27,7 @@ class Search(object):
                 feature = Feature("Reg-Ex Hit","Gene Sequence", match.start(),match.end() )
                 regexlist.addFeature(feature)
             Imports.con.addFeatureList(regexlist)
-            TreeView.tree.AddTreeNodes(TreeView.tree.searchResults, matches)
+            TreeView.AddTreeNodes(TreeView.tree.searchResults, matches)
 
 
 
@@ -36,5 +38,5 @@ class Search(object):
         genome = Imports.genome
         searchstring = genome.getSequence()
         pos = searchstring.index(patterns)
-        self.model.setRanges(pos-2000, pos+2000)
+        self.model.setRanges(pos-2500, pos+2500)
         self.model.setPosition(pos)
