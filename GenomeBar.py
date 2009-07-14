6cbd0a14c6c869713cc8a85b8031ae918fdba5c0
unknown <Mirco@.(none)>
2009-07-14 02:13:33 +0200

    winfix

--- GenomeBar.py
+++ GenomeBar.py
@@ -7,45 +7,35 @@ from GenomeModel import GenomeModel
 
 
 class GenomeBar(wx.Panel, IObserver):
-    """ jumps to position in genome-data by clicking """
 
     def __init__(self, model, *args, **kwargs):
-        """ initializes GenomeBar """
         wx.Panel.__init__(self, style=wx.BORDER_SUNKEN, *args, **kwargs)
 
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_SIZE, self.OnResize)
         self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
         self.width, self.height = self.GetSize()
-        
-        #background image
-        self.barImg = wx.Image("genomebar.jpg", type=wx.BITMAP_TYPE_ANY, index=-1)
-        self.barBmp = self.barImg.ConvertToBitmap()
-
         #pointer image
-        self.pointerImg = wx.Image("genomepointer.png", type=wx.BITMAP_TYPE_ANY, index=-1)
-        self.pointerBmp = self.pointerImg.ConvertToBitmap()
-        
+        #self.pointerImg = wx.Image("genomepointer.png", type=wx.BITMAP_TYPE_ANY, index=-1)
+        #self.pointerBmp = self.pointerImg.ConvertToBitmap()
+
         #cross-hair cursor
         self.cursor = wx.StockCursor(wx.CURSOR_CROSS)
         self.SetCursor(self. cursor)
-        
+
         self.model = model
         self.Centre()
         self.Show(True)
         self.drawBar()
 
 
-    def OnPaint(self, event):
-        """ redraws GenomeBar """
-        #self.drawBar()
-        pass
+    def OnResize(self, event):
+        self.drawBar()
 
 
     def OnLeftDown(self, event):
-        """ sets pointer to relative position
-            jumps to position in GenomeView """
         pos = event.GetPosition()
         genomePos = self.mousePosToGenomePos(pos[0])
+        #print "#",genomePos
         startpos = max(0, genomePos - 2500)
         endpos = min(Imports.genome.getSequenceLength(), genomePos + 2500)
 
@@ -56,8 +46,6 @@ class GenomeBar(wx.Panel, IObserver):
 
 
     def mousePosToGenomePos(self, mPos):
-        """ calculates position in genome from actual mouse position
-            returns integer (position in genome) """
         mPos = float(mPos)
         width = float(self.GetSize()[0])
         proportion = mPos / width
@@ -66,8 +54,6 @@ class GenomeBar(wx.Panel, IObserver):
 
 
     def genomePosToMousePos(self, gPos):
-        """ calculates position in genome-bar from actual genome position
-            returns integer (position in genome-bar) """
         gPos = float(gPos)
         width = float(Imports.genome.getSequenceLength())
         proportion = 0
@@ -80,8 +66,8 @@ class GenomeBar(wx.Panel, IObserver):
     def drawBar(self):
         """ draws GenomeBar """
         dc = wx.ClientDC(self)
-        self.width, self.height = self.GetSize()  
-        
+        self.width, self.height = self.GetSize()
+
         #draw background
         #self.barImg = self.barImg.Scale(self.width, self.height)
         #self.barBmp = self.barImg.ConvertToBitmap()
@@ -90,7 +76,7 @@ class GenomeBar(wx.Panel, IObserver):
         #dc.SetBrush(barBrush)
         dc.SetPen(wx.Pen('white'))
         dc.DrawRectangle(0, 0, self.width-3.5, self.height-3.5)
-        
+
         #draw scale
         dc.SetPen(wx.Pen('black'))
         for i in range(self.width):
@@ -108,13 +94,11 @@ class GenomeBar(wx.Panel, IObserver):
 
         #draw pointer
         pos = self.genomePosToMousePos(self.model.getPosition())
-        pos = pos - self.pointerImg.GetWidth()/2 +1
-        dc.DrawBitmap(self.pointerBmp, pos, 0)
-
+        #pos = pos - self.pointerImg.GetWidth()/2 +1
+        dc.DrawLine(pos, 0, pos, self.height)
 
     def update(self, source, object):
-        """ observer update-method """
         self.drawBar()
-        
-        
-        
+
+
+
